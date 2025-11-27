7a01aa5e8d66ad7f5f2bb8071601cc599a69cd57
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-11-27 18:41:31 +0530

    feat(project): add Project model

---
+++ backend/src/models/project.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+/** Mongoose document structure for a project. */
+export interface ProjectDocument extends Document {
+  name: string;
+  description: string | null; // Optional description for the project
+  emoji: string;
+  workspace: mongoose.Types.ObjectId;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/** Schema defining project metadata, workspace, and creator information. */
+const projectSchema = new Schema<ProjectDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    emoji: {
+      type: String,
+      required: false,
+      trim: true,
+      default: "📊",
+    },
+    description: { type: String, required: false },
+    workspace: {
+      type: Schema.Types.ObjectId,
+      ref: "Workspace",
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProjectModel = mongoose.model<ProjectDocument>("Project", projectSchema);
+export default ProjectModel;
