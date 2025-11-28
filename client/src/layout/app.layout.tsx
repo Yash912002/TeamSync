3a52109e6c3215c03e07e46a33b6529eb6105ee7
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-11-28 12:24:25 +0530

    feat(ui): add AppLayout with providers, sidebar, header, and dialogs

---
+++ client/src/layout/app.layout.tsx
@@ -0,0 +1,35 @@
+import Asidebar from "@/components/asidebar/asidebar";
+import Header from "@/components/header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import CreateWorkspaceDialog from "@/components/workspace/create-workspace-dialog";
+import CreateProjectDialog from "@/components/workspace/project/create-project-dialog";
+import { AuthProvider } from "@/context/auth-provider";
+import { Outlet } from "react-router-dom";
+
+/**
+ * Wraps the app with global providers and renders
+ * the sidebar, header, routed content, and global dialogs.
+ */
+const AppLayout = () => {
+  return (
+    <AuthProvider>
+      <SidebarProvider>
+        <Asidebar />
+        <SidebarInset className="overflow-x-hidden">
+          <div className="w-full">
+            <>
+              <Header />
+              <div className="px-3 lg:px-20 py-3">
+                <Outlet />
+              </div>
+            </>
+            <CreateWorkspaceDialog />
+            <CreateProjectDialog />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </AuthProvider>
+  );
+};
+
+export default AppLayout;
